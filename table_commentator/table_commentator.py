79f8621ddc77f7eb05569bdd501cad611b991b24
Artem <poznyshev.a.a@yandex.ru>
2025-01-11 00:19:50 +0400

    Исправлены ошибки, стабильная версия. Необходимо разобраться как использовать публичные методы возвращающие self для получения данных в чистом виде и создать метод для сохранения данных (бут то колонки или таблица или все вместе).

--- table_commentator/table_commentator.py
+++ table_commentator/table_commentator.py
@@ -5,12 +5,11 @@
     что приводит к потере метаданных таблицы (комментариев).
 """
 
-
 # ----------------------------------------------------------------------------------------------------------------------
-from typing import Union
+from typing import Union, List, Self
 # ---------------------------------- airflow
 # ---------------------------------- Импорт сторонних библиотек
-from sqlalchemy.engine import Engine
+from sqlalchemy.engine import Engine, Row
 from sqlalchemy import text
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
@@ -26,7 +25,7 @@ class TableCommentator:
     """
         Класс для создания комментариев к таблицам и колонкам после перезагрузки в них данных.
         Справка:
-        В Пандас используется метод сохранения данных, где под "капотом"  перед сохранением сначала удаляется таблица,
+        В Пандас используется метод сохранения данных, где под "капотом" перед сохранением сначала удаляется таблица,
         что приводит к потере метаданных таблицы (комментариев).
     """
 
@@ -43,10 +42,101 @@ class TableCommentator:
     # COMMENT ON COLUMN "schema"."table"."name_table" IS 'comment'
     SQL_SAVE_COMMENT_COLUMN = """COMMENT ON {0} "{1}"."{2}"."{3}" IS '{4}'"""  # COLUMN
 
+    SQL_GET_TABLE_COMMENTS = """
+        SELECT
+            comments.description AS description
+        FROM
+            pg_class AS all_entity
+        INNER JOIN
+            pg_description AS comments
+        ON 
+            all_entity.oid = comments.objoid 
+        AND 
+            comments.objsubid = 0 
+        AND 
+            all_entity.relname = '{table_name}'
+    """
+
+    SQL_GET_ALL_COLUMN_COMMENTS = """
+        SELECT
+            cols.attname AS column_name, 
+            comments.description AS description
+        FROM
+            pg_class AS all_entity
+        INNER JOIN
+            pg_description AS comments
+        ON 
+            all_entity.oid = comments.objoid 
+        AND 
+            comments.objsubid > 0 
+        AND 
+            all_entity.relname = '{table_name}'
+        INNER JOIN 
+            pg_attribute AS cols                    
+        ON
+            cols.attnum = comments.objsubid 
+        AND
+            cols.attrelid = all_entity.oid      
+    """
+
+    SQL_GET_COLUMN_COMMENTS_BY_INDEX = """
+        SELECT 
+            cols.attname AS column_name,       
+            comments.description AS description         
+        FROM 
+            pg_class AS all_entity
+        INNER JOIN 
+            pg_description AS comments              
+        ON 
+            all_entity.oid = comments.objoid
+        AND
+            comments.objsubid > 0
+        AND
+            all_entity.relname = '{table_name}'
+        INNER JOIN 
+            pg_attribute AS cols                  
+        ON
+            cols.attnum = comments.objsubid 
+        AND
+            cols.attrelid = all_entity.oid      
+        WHERE 
+             comments.objsubid IN ({columns})
+    """
+
+    SQL_GET_COLUMN_COMMENTS_BY_NAME = """
+        SELECT 
+            cols.attname AS column_name, 
+            comments.description AS description    
+        FROM 
+            pg_class AS all_entity
+        INNER JOIN 
+            pg_description AS comments             
+        ON
+            all_entity.oid = comments.objoid 
+        AND 
+            comments.objsubid > 0              
+        AND
+            all_entity.relname = '{table_name}'  
+        INNER JOIN 
+            pg_attribute AS cols                    
+        ON
+            cols.attnum = comments.objsubid 
+        AND
+            cols.attrelid = all_entity.oid
+        WHERE
+             cols.attname IN ({columns})
+    """
+
     def __init__(self, engine: Engine, name_table: str, schema: str):  # | AsyncEngine
         self.engine = self._validator(engine, Engine)
-        self.name_table = self._validator(name_table, str)
+        self.name_table: str = self._validator(name_table, str)
         self.schema = self._validator(schema, str)
+        # ***
+        self.table_comments_dict: Union[dict[str, str], None] = None
+        self.column_comments_dict: Union[dict[str, str], None] = None
+        self.all_comments_table_dict: Union[dict[str, str], None] = None
+
+    # ***
 
     @staticmethod
     def _validator(value: any, check_type: any) -> any:
@@ -58,27 +148,110 @@ class TableCommentator:
         else:
             raise TypeError(f'Недопустимый тип данных для аргумента: {value}.')
 
-    def recorder(self, sql: Union[str, text]) -> None:  # sql | text # executer | list[tuple] Union[int, str]
+    def _check_all_elements(self, check_type: type, args_elements: any) -> bool:  # *args_elements
         """
-           Метод выполняет запросы к базе данных на запись.
+            Метод для проверки соответствия условию всех элементов  в выборке.
+            На входе:
+                - check_type: тип данных для проверки (например, str, int).
+                - *args_elements: произвольное количество аргументов для проверки.
+            На выходе: True, если все элементы соответствуют типу; иначе False.
         """
-        engine: Engine = self.engine
 
-        if isinstance(sql, str):
-            sql = text(sql)
+        # Валидация переданного аргумента (соответствует типу данных) для дальнейшей проверки:
+        valid_type = self._validator(check_type, type)
+        print(f"check_type: {check_type}, args_elements: {args_elements}")
 
-        # result.fetchall() if result else None - не верно
+        # Проверяем, все ли элементы имеют один и тот же тип:
+        return all(isinstance(element, valid_type) for element in args_elements)
 
-        with engine.connect() as conn:
-            with conn.begin():
-                conn.execute(sql)
+    def _sql_formatter(self, sql: str, params: tuple = None) -> str:  #
+        """
+            Служебный вложенный метод для форматирования sql (передачи необходимых параметров в запрос).
+                На входе:
+                На выходе: .
+        """
 
-            # tuple_list = result.fetchall()
+        valid_sql = self._validator(sql, str)
 
-        # if tuple_list:
-        #     return tuple_list
-        # else:
-        #     return None
+        # Если указаны параметры, тогда форматируем:
+        if params:
+            # valid_table_name = self._validator(table_name, str) - устарело
+            valid_params = self._validator(params, tuple)
+
+            # Имя колонки (сохраняем последовательность через запятую в кавычках:
+            columns_string = ', '.join(f"'{columns}'" for columns in valid_params)
+
+            # Форматирование sql_str с учетом параметров:
+            mutable_sql = valid_sql.format(table_name=self.name_table, columns=columns_string)
+        else:
+            mutable_sql = valid_sql.format(table_name=self.name_table)
+
+        return mutable_sql
+
+    def _mutation_sql_by_logic(self, param_column_index_or_name: tuple[Union[int, str]]) -> str:  # , table_name: str
+        """
+           Метод изменения sql запроса в зависимости от переданных параметров
+           (содержит логику вариантов форматирования).
+                На входе:
+                    - param_column_index_or_name: либо только индексы либо имена колонок.
+                    - table_name: мя таблицы к которой выполняется запрос.
+                На выходе: готовый запрос.
+        """
+
+        # Если были указаны параметры на конкретные колонки:
+        if param_column_index_or_name:
+
+            #  Проверка первого элемента на тип данных (исключает дублирования проверок):
+            if isinstance(param_column_index_or_name[0], str):  # check_first_itm_type
+
+                # Если вводим имя колонки (хотим получить комментарий для колонки по ее имени):
+                if self._check_all_elements(str, param_column_index_or_name):
+
+                    # Форматирование sql_str с учетом параметров:
+                    mutable_sql = self._sql_formatter(
+                        sql=self.SQL_GET_COLUMN_COMMENTS_BY_NAME,
+                        # table_name=self.name_table, - устарело.
+                        params=param_column_index_or_name
+                    )
+
+                # Если не все элементы имеют один и тот же тип или недопустимые:
+                else:
+                    raise TypeError(
+                        f'Переданные аргументы не соответствуют единому типу данных, '
+                        f'должны быть либо только str (имена колонок),'
+                        f' либо только int (индексы колонок).'
+                    )
+
+            elif isinstance(param_column_index_or_name[0], int):
+
+                # Если вводим индекс колонки (хотим получить комментарий для колонок по индексам):
+                if self._check_all_elements(int, param_column_index_or_name):
+
+                    # Форматирование sql_str с учетом параметров:
+                    mutable_sql = self._sql_formatter(
+                        sql=self.SQL_GET_COLUMN_COMMENTS_BY_INDEX,
+                        # table_name=self.name_table, - устарело.
+                        params=param_column_index_or_name
+                    )
+                # Если не все элементы имеют один и тот же тип или недопустимые:
+                else:
+                    raise TypeError(
+                        f'Переданные аргументы не соответствуют единому типу данных, '
+                        f'должны быть либо только str (имена колонок),'
+                        f' либо только int (индексы колонок).'
+                    )
+
+            # Если элементы не соответствуют допустимым типам данных:
+            else:
+                raise TypeError(
+                    f'Переданные аргументы должны быть либо str (имена колонок), либо int (индексы колонок), '
+                    f'другие типы данных недопустимы.'
+                )
+        else:
+            # Если не вводим ни имя колонки, ни индекс (хотим получить все комментарии ко всем существующим колонкам):
+            mutable_sql = self._sql_formatter(sql=self.SQL_GET_ALL_COLUMN_COMMENTS)  # По умолчанию!
+
+        return mutable_sql
 
     def _create_comment(self, type_comment: str, comment: str, name_column: str = None) -> None:
         """
@@ -111,6 +284,99 @@ class TableCommentator:
 
         self.recorder(mutable_sql_variant)
 
+    def _get_table_comments(self) -> dict[str, str]:  # , table_name: str
+        """
+            Метод (приватный) для получения комментариев к таблицам.
+            На выходе: str, - строка с комментарием к таблице.
+        """
+
+        # Проверка корректности типа данных для введенного значения, переменная - имея таблицы:
+        # check_name_table = self._validator(table_name, str) - устарело
+        mutable_sql = self.SQL_GET_TABLE_COMMENTS.format(table_name=self.name_table)
+
+        # Получаем сырые данные после запроса (список кортежей):
+        table_comment_tuple_list: list[tuple] = self.reader(sql=mutable_sql)
+
+        # Преобразуем (обращаемся к первому элементу в списке (к кортежу, будет всего один всегда) и распаковываем:
+        if table_comment_tuple_list:
+            table_comment = table_comment_tuple_list[0][0]
+        else:
+            table_comment = ''  # Если комментарий отсутствует, возвращаем пустую строку.
+
+        return {'table': table_comment}
+
+    def _get_column_comments(self, *column_index_or_name: Union[int, str]) -> dict[str, dict]:  # Union[int, str]
+        """
+            Метод для получения комментариев к колонкам либо по индексу, либо по имени колонки.
+            Важно:
+                Если передать в параметры индекс и имя колонки - это вызовет исключение!
+                Метод обрабатывает параметры только одного типа (либо только индексы, либо имена колонок).
+            На выходе:
+                словарь с вложенным словарем с комментариями: {'columns': {column: comments}}
+
+        """
+
+        # Значение по умолчанию - получаем все комментарии к колонкам в таблице (без указания индекса или имени):
+        param_column_index_or_name = None or column_index_or_name
+
+        # Проверка корректности типа данных для введенного значения, переменная - имея таблицы:
+        # check_name_table = self._validator(self.name_table, str) -  устарело
+
+        # Изменение sql запроса в зависимости от переданных параметров
+        mutable_sql = self._mutation_sql_by_logic(param_column_index_or_name)  # , table_name: str
+
+        # Преобразование sql_str в тип данных sqlalchemy:
+        final_sql = text(mutable_sql)
+
+        column_comments_tuple_list: list[tuple] = self.reader(sql=final_sql)
+
+        # Генерация словаря из списка кортежей:
+        # Распаковывает кортеж из 2-х элементов (1, 'Alice') принимая первый за key и второй за value:
+        _column_comments_dict = {key: value for key, value in column_comments_tuple_list}
+
+        return {'columns': _column_comments_dict}  # {'columns': {column: comments} }
+
+    #  **
+
+    def recorder(self, sql: Union[str, text]) -> None:  # sql | text # executer | list[tuple] Union[int, str]
+        """
+           Метод выполняет запросы к базе данных на запись.
+        """
+        engine: Engine = self.engine
+
+        if isinstance(sql, str):
+            sql = text(sql)
+
+        with engine.connect() as conn:
+            with conn.begin():
+                conn.execute(sql)
+
+    def reader(self, sql: Union[str, text]) -> list[tuple]:  # dict[str, text] list[Row]
+        """
+            Метод выполняет запросы к базе данных на чтение и возвращает данные.
+            На входе: sql - SQL-запрос в виде строки или объекта sqlalchemy.text.
+            На выходе: список картежей или [].
+                Возвращает все строки результата в виде списка кортежей.
+                Пример результата:
+                    [
+                        (1, 'Alice'),
+                        (2, 'Bob'),
+                        (3, 'Charlie')
+                    ]
+            Примечание:
+                При работе с «чистым» SQL через session.execute() результат аналогичен fetchall()
+                через engine.connect() и возвращает объекты Row.
+        """
+
+        engine: Engine = self.engine
+
+        with engine.connect() as conn:
+            with conn.begin():
+                result = conn.execute(sql)
+                # tuple_list = result.fetchall()  # Возвращает объект Row
+                tuple_list = [tuple(row) for row in result.fetchall()]  # Возвращает tuple_list
+        return tuple_list  # Может заменить на self
+
     def row_sql_recorder(self, sql: str) -> None:
         """
             Метод для создания сырых запросов на запись данных.
@@ -118,28 +384,30 @@ class TableCommentator:
 
         self.recorder(sql)
 
-    def table_comment(self, comment: str) -> None:
+    # *
+
+    def set_table_comment(self, comment: str) -> None:
         """
             Метод для создания комментариев к TABLE.
         """
 
         self._create_comment(type_comment='TABLE', comment=comment)
 
-    def view_comment(self, comment: str) -> None:
+    def set_view_comment(self, comment: str) -> None:
         """
             Метод для создания комментариев к VIEW.
         """
 
         self._create_comment(type_comment='VIEW', comment=comment)
 
-    def materialized_comment(self, comment: str) -> None:
+    def set_materialized_comment(self, comment: str) -> None:
         """
             Метод для создания комментариев к MATERIALIZED VIEW.
         """
 
         self._create_comment(type_comment='MATERIALIZED', comment=comment)
 
-    def column_comment(self, **comments_columns: str) -> None:
+    def set_column_comment(self, **comments_columns: str) -> None:
         """
             Метод для создания комментариев к COLUMN.
         """
@@ -147,15 +415,50 @@ class TableCommentator:
         for key_name_column, value_comment in comments_columns.items():
             self._create_comment(type_comment='COLUMN', comment=value_comment, name_column=key_name_column)
 
+    #  *
 
+    def get_table_comments(self) -> Self:  # или 'TableCommentator' на Python < 3.11., table_name: str
+        """
+            Метод (публичный) для получения комментариев к таблицам.
+            На выходе: str, - строка с комментарием к таблице.
+        """
 
+        self.table_comments_dict = self._get_table_comments()
+        return self
 
+    def get_column_comments(self, *column_index_or_name: str) -> Self:  # -> dict[str, text] , table_name: str
+        """
+            Метод (публичный) для получения комментариев к колонкам либо по индексу, либо по имени колонки.
+                Если передать в параметры индекс и имя колонки - это вызовет исключение!
+                Метод обрабатывает параметры только одного типа (либо только индексы, либо имена колонок).
+            На выходе - словарь с комментариями
 
+        """
 
+        self.column_comments_dict = self._get_column_comments(column_index_or_name)
+        return self
 
+    def get_all_comments(self) -> Self:  # , schema: str
+        """
+            Метод для получения всех комментариев к колонкам и таблицам.
+            На выходе - словарь типа:
+                result = {
+                    'table': 'set_table_comment',
+                    'columns': {
+                        'column1': 'column1_comment',
+                        'column2': 'column2_comment',
+                        'column3': 'column3_comment',
+                    }
+                }.
+        """
 
+        # Получение всех комментариев:
+        table_comment = self._get_table_comments()
+        column_comments_dict = self._get_column_comments()
 
+        # Преобразование полученных данных в единый словарь:
+        # на выходе: {'table': set_table_comment, 'columns': column_comments_dict}
+        self.all_comments_table_dict = table_comment | column_comments_dict
+        return self
 
 # ----------------------------------------------------------------------------------------------------------------------
-# comments = TableCommentator(engine=engine_, name_table='', schema='')
-# comments.table_comment()
